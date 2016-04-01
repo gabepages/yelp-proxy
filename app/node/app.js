83eafc3c6f5b3133675ea2aaae1b899ac315c6b1
annaimpson <annaimpson@gmail.com>
2016-04-01 14:46:19 -0400

    deleted characters

--- app/node/app.js
+++ app/node/app.js
@@ -2,10 +2,9 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var cors = require('cors');
-​
+
 app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
-​
-​
+
 app.get('/api', function(req, res){ // listens for request on /api route
   var lat = req.query.lat; // grabs lat and lng queries from the request object
   var lng = req.query.lng;
@@ -21,7 +20,7 @@ app.get('/test', function(req, res){ // listens for request on /api route
  console.log('working!');
  res.send('working!'); // if no errors, send the body of data back to front end
 });
-​
+
 var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server running on port %d');
